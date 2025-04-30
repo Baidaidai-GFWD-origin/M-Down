041936d2f40b2f656d9b6f64ba0118b697a7a797
Baidaidai_GFWD <1302064396@qq.com>
2025-04-30 18:55:26 +0800

    add new-gen style && remove useless components

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,53 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import FigmaIcon from './components/icnos/FigmaIcon.vue';
+  import GithubIcon from './components/icnos/GithubIcon.vue';
+  import { ref } from 'vue';
+
+  let currentLeftComponent = ref(FigmaIcon)
+  let currentMiddleComponent = ref(FigmaIcon)
+  let currentRightComponent = ref(FigmaIcon)
+
+  function changeLeft(param = FigmaIcon){
+    currentLeftComponent.value = param
+  }
+  function changeMiddle(param = FigmaIcon){
+    currentMiddleComponent.value = param
+  }
+  function changeRight(param = FigmaIcon){
+    currentRightComponent.value = param
+  }
+
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="menu-whole-container" class=" h-screen w-screen flex flex-col">
+    <div id="meal-container" class=" w-full h-56 bg-teal-50 flex justify-center pt-10">
+      <component :is="currentLeftComponent" class="relative  translate-x-5 z-20" id="component-selecter-left"></component>
+      <component :is="currentMiddleComponent" class="relative  translate-y-5 z-10" id="component-selecter-middle"></component>
+      <component :is="currentRightComponent" class="relative  -translate-x-5 z-20" id="component-selecter-right"></component>
     </div>
-  </header>
-
-  <RouterView />
+    <div id="meal-selector" class="bg-violet-50 grow flex flex-col w-full justify-around items-center">
+      <div id="left-selector">
+        <div class=" w-28 h-8 rounded-2xl bg-violet-200 flex">
+          <p class="m-auto">Left Selector</p>
+        </div>
+        <button class=" m-4 w-16 h-12 rounded-2xl bg-violet-200" @click="changeLeft()">Figma</button>
+        <button class=" m-4 w-16 h-12 rounded-2xl bg-violet-200" @click="changeLeft(GithubIcon)">Github</button>
+      </div>
+      <div id="mid-selector">
+        <div class=" w-32 h-8 rounded-2xl bg-violet-200 flex">
+          <p class="m-auto">Middle Selector</p>
+        </div>
+        <button class=" m-4 w-16 h-12 rounded-2xl bg-violet-200" @click="changeMiddle()">Figma</button>
+        <button class=" m-4 w-16 h-12 rounded-2xl bg-violet-200" @click="changeMiddle(GithubIcon)">Github</button>
+      </div>
+      <div id="right-selector">
+        <div class=" w-32 h-8 rounded-2xl bg-violet-200 flex">
+          <p class="m-auto">Right Selector</p>
+        </div>
+        <button class=" m-4 w-16 h-12 rounded-2xl bg-violet-200" @click="changeRight()">Figma</button>
+        <button class=" m-4 w-16 h-12 rounded-2xl bg-violet-200" @click="changeRight(GithubIcon)">Github</button>
+      </div>
+      
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
